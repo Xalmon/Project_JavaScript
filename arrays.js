b037aa35980e262d3fdd63e9505f1f58b5f6c72c
Xalmon <xalmonoyefule@gmail.com>
2023-10-04 16:54:20 +0100

    4/10/2023

--- arrays.js
+++ arrays.js
@@ -1,15 +1,25 @@
-let arr1 = new Array(5);
-let arr2 = [1, 2, 4, 4, 6];
-let arr3 = new Array(5, 4, 2, 5, 6);
+// let arr1 = new Array(5);
+// let arr2 = [1, 2, 4, 4, 6, "seven"];
+// let arr3 = new Array(5, 4, 2, 5, 6);
+// let arr4 = new Array(8, 3, 4);
 
-arr1 = [3, "4", 7, "3", 8, {name : "Daniel"}]
-arr.shift();
-console.log(arr1.unshift(23));
-console.log(arr1);
-arr.pop();
-arr1.push(45);
-console.log(arr1);
 
-Array.prototype.laugh = "lol";
-console.log(arr1.laugh);
+// arr1 = [3, "4", 7, "3", 8, {name : "Daniel"}];
+// arr1.shift();
+// console.log(arr2);
+// console.log(arr1.unshift(23));
+// console.log(arr1);
+// arr3.pop();
+// arr1.push(45);
+// console.log(arr1);
 
+// Array.prototype.laugh = "lol";
+// console.log(arr1.laugh);
+
+// let typedArray = new Int8Array(5); 
+// typedArray[0] = "One";
+// typedArray[0] = 1;
+// console.log(typedArray);
+
+let array = [[1,2], [3,4]];
+console.table(array);
