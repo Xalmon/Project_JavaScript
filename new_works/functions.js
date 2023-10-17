99d7a5b5aeec3445207e254a4b813cb878ea72ef
Xalmon <xalmonoyefule@gmail.com>
2023-10-17 16:27:04 +0100

    rework and new

---
+++ new_works/functions.js
@@ -0,0 +1,13 @@
+function sum(x, y){
+    return x + y;
+}
+
+function square(z){
+    return z * z;
+}
+
+function sumOfSquare(x, y){
+    return sum(square(x), square(y));
+}
+
+console.log(sumOfSquare(2, 3))
